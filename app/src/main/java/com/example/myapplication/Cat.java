ea4dfc16c6a6ed3799cd42c1feaf21b45ff74afb
Andrew <moysin8@mail.ru>
2022-10-26 10:44:11 +0300

    fggdf

--- app/src/main/java/com/example/myapplication/Cat.java
+++ app/src/main/java/com/example/myapplication/Cat.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import android.util.Log;
 
-public class Cat {
+public class Cat extends  Animal{
     int age;
     String name;
     String color;
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +19,24 @@ public class MainActivity extends AppCompatActivity {
 
         Cat murzik = new Cat(2, "Murza","Red","Cote");
         murzik.talk();
+        murzik.breathe();
 
         Cat glasha = new Cat();
         glasha.age = 1;
         glasha.name = "Glasha";
         glasha.breed = "Cattt";
         glasha.color = "White";
+        glasha.number0fLegs = 4;
+        Log.i("number0fLegs", String.valueOf(glasha.number0fLegs));
         glasha.talk();
+
+        Puma puma = new Puma();
+        puma.name = "Leo";
+        puma.breathe();
+        puma.talk();
+        puma.isAlive = true;
+        Log.i("isAlive", String.valueOf(puma.isAlive));
+        Log.i("breathe()", "My name is " + puma.name +
+                " and " + puma.breathe());
     }
 }
